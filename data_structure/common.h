dfc35242e0d5a9a5db3245205eea96fb6f94de12
rafaelbusch <rafaelbusch@hotmail.com>
2015-10-27 17:22:54 -0200

    meu primeiro commit

---
+++ data_structure/common.h
@@ -0,0 +1,38 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#define CLEAR		"\e[H\e[2J"
+#define SKIP_LINE 	"%*[^\n]"
+#define RED		"\e[31m"
+#define BLUE		"\e[34m"
+#define GREEN		"\e[32m"
+#define NORMAL		"\e[0m"
+#define SIZE_NAME	100
+#define SIZE_ADDRESS	100
+#define SIZE_EMAIL	100
+#define TIME_IN_SECONDS	1
+
+typedef struct tree tree_t;
+
+struct tree {
+       char name[SIZE_NAME];
+       char address[SIZE_ADDRESS];
+       int phone;
+       char email[SIZE_EMAIL];
+       struct tree *left;
+       struct tree *right;
+};
+
+/* utils.c */
+void menu(int *option);
+void tittle(void);
+tree_t *get_info();
+void get_name(char name[]);
+void get_address(char address[]);
+void get_email(char email[]);
+void get_phone(int *phone);
+
+/* tree.c */
+void insert(tree_t **root, tree_t **newNode);
+void print(tree_t *root);
+#endif /* _COMMON_H_DEFINED_ */
---
+++ data_structure/tree.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdio_ext.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "common.h"
+
+void insert(tree_t **root, tree_t **newNode)
+{
+	if(*root == NULL) 
+		*root = *newNode;
+	else {
+		if(strcasecmp((*root)->name, (*newNode)->name) > 0)
+			insert(&(*root)->left, newNode);
+		else {
+			if(strcasecmp((*root)->name, (*newNode)->name) < 0)
+				insert(&(*root)->right, newNode);
+			else {
+				if(strcmp((*root)->name, (*newNode)->name) == 0)
+					fprintf(stderr, "Nome já existe na agenda!\n");
+			}
+		}
+	} 
+}
+
+void print(tree_t *root)
+{
+	if(root != NULL) {
+		print((root)->left);
+		printf("Nome do contato: %s",(root)->name);
+		printf("Endereço: %s",(root)->address);
+		printf("E-mail: %s",(root)->email);
+		printf("Telefone: %d",(root)->phone);
+		print((root)->right);
+	}
+}
+
+
